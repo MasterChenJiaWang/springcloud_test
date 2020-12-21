8dc0831638885fe7d62a97056bf47bf0e7fb1523
chenjiawang <372070693@qq.com>
2020-12-21 17:44:59 +0800

    测试eureka集群 多注册中心

---
+++ server2/src/test/java/com/chen/test/server1/Server1ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.chen.test.server1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Server1ApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
